cb300694caf75715b6a4800c1d1bf4b92d1d805e
Joe McGrath <joe.mcgrath@fanduel.com>
2018-07-28 18:58:56 +0100

    make channels responsive

---
+++ src/features/responsiveChannels.js
@@ -0,0 +1,12 @@
+const toggleClass = 'refined-discord_mobile-toggle'
+
+window.addEventListener('load', () => {
+	const toggleElement = document.createElement('div')
+	toggleElement.className = toggleClass
+	toggleElement.innerHTML = 'toggle channels'
+
+	toggleElement.addEventListener('click', () => {
+		toggleElement.classList.toggle('show-channels')
+	})
+	document.body.insertAdjacentElement('afterbegin', toggleElement)
+})
